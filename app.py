3695af799f966a1ca163b40a1f1997c930c2078e
warlikeprinse <bhausaheb.sublimetechnocorp@gmail.com>
2023-07-03 19:03:02 +0530

    added init code

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask , jsonify , request
+from connect import connectiontest; 
+
+
+app = Flask(__name__)
+
+client = connectiontest();
+
+
+
+@app.route("/user/set", methods=["GET", "POST"  , "PUT" , "DELETE"] )
+def setuser():
+    userList = [] 
+    dbConnect  =  client.get_database("sample_mflix").get_collection("Users")
+    if request.method == 'DELETE': 
+        userId = request.form.get("idNo")
+        if userId is None : 
+            return "Please provide proper information"
+        dbConnect.find_one_and_delete({"id":int(userId)})
+        return f"{userId} is deleted successfully"
+
+    if request.method == 'POST':
+        userId = request.form.get("idNo")
+        username = request.form.get("username")
+        age = request.form.get("age")
+        email = request.form.get("email")
+        if userId is None or username is None or age is None or email is None : 
+            return "Please provide proper information"
+        
+        dbConnect.insert_one({ 
+            "id": int(userId),
+            "age":int(age),
+            "email":email,
+            "username":username,
+        })
+        for data in dbConnect.find({"id":int(userId)}):
+            userList.append({ 
+                "id":str( data["_id"]),
+                "idNo":str(data["id"]),
+                "age":str(data["age"]),
+                "email":data["email"],
+                "username":data["username"],
+            })
+        return userList
+@app.route("/user", methods=["GET", "POST"] )
+def getuser():
+    userList = [] 
+    dbConnect  =  client.get_database("sample_mflix").get_collection("Users")
+    if request.method == 'POST':
+        userId = request.form.get("userId")
+       
+        for data in dbConnect.find({"id":int(userId)}):
+            userList.append({ 
+                "id":str( data["_id"]),
+                "idNo":str(data["id"]),
+                "age":str(data["age"]),
+                "email":data["email"],
+                "username":data["username"],
+            })
+    if request.method == 'GET':
+         for data in dbConnect.find():
+            userList.append({ 
+                "id":str( data["_id"]),
+                "idNo":str(data["id"]),
+                "age":str(data["age"]),
+                "email":data["email"],
+            })
+    return userList
+
+
+@app.route("/movies", methods=["GET", "POST"] )
+def getMovieData():
+    movieList = [] 
+    limit = 10
+    if request.method == 'POST':
+        limit = request.form.get("limit")
+       
+        if limit is None:
+            return "Please enter an integer value."
+       
+        dbConnect  =  client.get_database("sample_mflix").get_collection("movies")
+        for data in dbConnect.find().limit(int(limit)):
+            movieList.append({
+            "_id": str( data["_id"]),
+            "plot": data["plot"],
+            "num_mflix_comments": data["num_mflix_comments"],
+            "cast": data["cast"],
+            "released": data["released"],
+            "lastupdated": data["lastupdated"]
+            })
+     
+        return jsonify(movieList)
+    if request.method == 'GET':
+        dbConnect  =  client.get_database("sample_mflix").get_collection("movies")
+        for data in dbConnect.find().limit(limit):
+            movieList.append({
+            "_id": str( data["_id"]),
+            "plot": data["plot"],
+            "num_mflix_comments": data["num_mflix_comments"],
+            "cast": data["cast"],
+            "released": data["released"],
+            "lastupdated": data["lastupdated"]
+            })
+        return jsonify(movieList)
+
+if __name__ == "__main__":
+    app.run(debug=True , host="0.0.0.0")
---
+++ connect.py
@@ -0,0 +1,22 @@
+from pymongo.mongo_client import MongoClient
+import os
+from dotenv import load_dotenv
+
+
+def connectiontest():
+# Load the .env file
+    load_dotenv()
+
+    # Replace the placeholder with your Atlas connection string
+    # uri = "<connection string>"
+
+    # Create a new client and connect to the server
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        return client
+    except Exception as e:
+        print(e)
