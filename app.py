e002a79e43799c4cb0d6e5ab5b9c2ec38187541e
PratikYerunkar <yerunkarpratik2260@gmail.com>
2023-12-13 17:27:51 +0530

    add api for test report

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask , jsonify , request
 from connect import connectiontest; 
-from Whatpy import  webdmclose 
+# from Whatpy import  webdmclose 
 import datetime
+import random
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 client = connectiontest();
 
-
-
+@app.route("/hello", methods=["GET", "POST"  , "PUT" , "DELETE"] )
+def hello():
+    print("hello world")
+    return "hello world" 
+    
+    
 @app.route("/user/set", methods=["GET", "POST"  , "PUT" , "DELETE"] )
 def setuser():
     userList = [] 
@@ -43,6 +49,8 @@ def setuser():
                 "username":data["username"],
             })
         return userList
+    
+    
 @app.route("/user", methods=["GET", "POST"] )
 def getuser():
     userList = [] 
@@ -104,16 +112,98 @@ def getMovieData():
             })
         return jsonify(movieList)
     
-@app.route("/sendwamsg")
-def sendwa():
-    current_time = datetime.datetime.now()
-    print(current_time)
-    # sendwhatmsg_instantly("+919819535276", "Message-2", current_time.hour, current_time.minute+1)
-    webdmclose("+919819535276", "Message-2");
-    return f"{current_time.hour}:{current_time.minute}"
+# @app.route("/sendwamsg")
+# def sendwa():
+#     current_time = datetime.datetime.now()
+#     print(current_time)
+#     # sendwhatmsg_instantly("+919819535276", "Message-2", current_time.hour, current_time.minute+1)
+#     webdmclose("+919819535276", "Message-2");
+#     return f"{current_time.hour}:{current_time.minute}"
 
+bananaCategory = ["All Green", "Light Green", "More Yellow Than Green", "Yellow With Green Tips", "Full Yellow", "Full Yellow With Spots"]
+tomatoCategry = ["Mature Green", "Breaker", "Turning", "Pink", "Light Red", "Red Ripe"]
+@app.route("/categoryData", methods=["GET", "POST", "PUT", "DELETE"] )
+def category():
+    categoryList = [""]
+    returnList = []
+    dbConnect = client.get_database("flask_data").get_collection("testData")
+    
+    # Add Data
+    if request.method == 'POST':
+        randomList = []
+        typeValue = request.form.get("type")
+        if typeValue == "banana":
+            categoryList = bananaCategory
+        elif typeValue == "tomato":
+            categoryList = tomatoCategry
+            
+        for i in range(6):
+            randomList.append(random.randint(1, 1000))
+        print(randomList)
+        categoryRandomValue = random.choice(categoryList)
+        addData = dbConnect.insert_one({ 
+            "r1": randomList[0],
+            "r2": randomList[1],
+            "r3": randomList[2],
+            "r4": randomList[3],
+            "r5": randomList[4],
+            "r6": randomList[5],
+            "typeValue": typeValue,
+            "category": str(categoryRandomValue)
+        })
+        returnList.append({
+            "_id": str(addData.inserted_id),
+            "r1":randomList[0],
+            "r2":randomList[1],
+            "r3":randomList[2],
+            "r4":randomList[3],
+            "r5":randomList[4],
+            "r6":randomList[5],
+            "typeValue": typeValue,
+            "category": str(categoryRandomValue)
+        })
+        return returnList
+    
+    # Get Data
+    if request.method == 'GET':
+        resultData = []
+        findId = request.form.get("findId")
+        for data in dbConnect.find({"_id": ObjectId(findId)}):
+            resultData.append({ 
+                "r1":data["r1"],
+                "r2":data["r2"],
+                "r3":data["r3"],
+                "r4":data["r4"],
+                "r5":data["r5"],
+                "r6":data["r6"],
+                "typeValue":data["typeValue"],
+                "category":data["category"]
+            })
+        return resultData
+    
+    # Update Data
+    if request.method == 'PUT':
+        findId = request.form.get("findId")
+        typeValue = request.form.get("type")
+        
+        updateData = dbConnect.find_one({"_id": ObjectId(findId)}, {"_id":0})
+        print("updateData\n", updateData)
+        updateData['_id'] = str(ObjectId())
+        collections = {
+            "banana": client.get_database("flask_data").get_collection("banana"),
+            "tomato": client.get_database("flask_data").get_collection("tomato")
+        }
+
+        if typeValue in collections:
+            dbConnect = collections[typeValue]
+            dbConnect.insert_one(updateData)
+        print(dbConnect)
+        
+        return updateData
+      
+    
 
 if __name__ == "__main__":
-    app.run(debug=True )
+    app.run(debug=True , host="0.0.0.0")
 
 
--- connect.py
+++ connect.py
@@ -5,13 +5,19 @@ from dotenv import load_dotenv
 
 def connectiontest():
 # Load the .env file
-    load_dotenv()
+    # load_dotenv()
 
     # Replace the placeholder with your Atlas connection string
     # uri = "<connection string>"
 
     # Create a new client and connect to the server
-    client = MongoClient(os.environ.get("MONGODB_URI"))
+    # client = MongoClient(os.environ.get("MONGODB_URI"))
+    # client = MongoClient("mongodb+srv://test:sublimetechnocorp@cluster0.0aaafyp.mongodb.net")
+    client = MongoClient(
+        host="mongodb://localhost:27017/",
+        port=27017,
+        authSource="admin"
+    )
 
     # Send a ping to confirm a successful connection
     try:
@@ -20,3 +26,4 @@ def connectiontest():
         return client
     except Exception as e:
         print(e)
+        print("Unable to connect to the MongoDB server.")
